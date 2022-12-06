69752582e2203b3558ee624bca03bf7c3d13792e
Paillat <git@electronique.cc>
2022-12-06 10:06:20 +0100

    More complete help commands

--- code/code.py
+++ code/code.py
@@ -239,11 +239,11 @@ async def info(ctx):
 async def advanced_help(ctx):
     debug(f"The user {ctx.author} ran the advanced_help command in the channel {ctx.channel} of the guild {ctx.guild}, named {ctx.guild.name}")
     embed = discord.Embed(title="Advanced Help", description="Here is the advanced help page", color=0x00ff00)
-    embed.add_field(name="max_tokens", value="The maximum number of tokens to generate. Higher values will result in more coherent text, but will take longer to complete. (default: 50)", inline=False)
-    embed.add_field(name="temperature", value="The higher the temperature, the crazier the text (default: 0.9)", inline=False)
+    embed.add_field(name="temperature", value="The higher the temperature, the more likely the model will take risks. Conversely, a lower temperature will make the model more conservative. The default value is 0.9", inline=False)
+    embed.add_field(name="max_tokens", value="The maximum number of tokens to generate. Higher values will result in more coherent text, but will take longer to complete. (default: 50). Lower values will result in somentimes cutting off the end of the answer, but will be faster.", inline=False)
     embed.add_field(name="frequency_penalty", value="The higher the frequency penalty, the more new words the model will introduce (default: 0.0)", inline=False)
     embed.add_field(name="presence_penalty", value="The higher the presence penalty, the more new words the model will introduce (default: 0.0)", inline=False)
-    embed.add_field(name="prompt_size", value="The number of messages to use as a prompt (default: 5)", inline=False)
+    embed.add_field(name="prompt_size", value="The number of messages to use as a prompt (default: 5). The more messages, the more coherent the text will be, but the more it will take to generate and the more it will cost.", inline=False)
     #add a footer
     embed.set_footer(text="Made by @Paillat#0001")
     await ctx.respond(embed=embed, ephemeral=True)
