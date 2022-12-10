9921f002d4f09b5b1bd40094c15edeac4d0bb767
Paillat <git@electronique.cc>
2022-12-10 01:52:17 +0100

    Update chat.py

--- code/cogs/chat.py
+++ code/cogs/chat.py
@@ -40,7 +40,7 @@ class Chat (discord.Cog) :
         #check if the bot hasn't been used more than 5000 times in the last 24 hours (uses_count_today)
         c.execute("SELECT uses_count_today FROM data WHERE guild_id = ?", (message.guild.id,))
         debug(f"uses_count_today: {c.fetchone()[0]}")
-        if c.fetchone()[0] >= max_uses:
+        if int(c.fetchone()[0]) >= max_uses:
             debug(f"The bot has been used more than {max_uses} times in the last 24 hours in this guild. Please try again in 24h.")
             await message.channel.send(f"The bot has been used more than {max_uses} times in the last 24 hours in this guild. Please try again in 24h.")
             return
