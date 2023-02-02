06df46d33418498cc7c347312f0f75cc27543caa
Paillat <git@electronique.cc>
2023-02-02 22:05:03 +0100

    Update config.py

--- code/config.py
+++ code/config.py
@@ -15,6 +15,8 @@ connp = sqlite3.connect('../database/premium.db')
 cp = connp.cursor()
 
 c.execute('''CREATE TABLE IF NOT EXISTS data (guild_id text, channel_id text, api_key text, is_active boolean, max_tokens integer, temperature real, frequency_penalty real, presence_penalty real, uses_count_today integer, prompt_size integer, prompt_prefix text, tts boolean, pretend_to_be text, pretend_enabled boolean)''')
+#we delete the moderation table and create a new one
+c.execute('''DROP TABLE IF EXISTS moderation''')
 c.execute('''CREATE TABLE IF NOT EXISTS moderation (guild_id text, logs_channel_id text, is_enabled boolean, mod_role_id text)''')
 cp.execute('''CREATE TABLE IF NOT EXISTS data (user_id text, guild_id text, premium boolean)''')
 cp.execute('''CREATE TABLE IF NOT EXISTS channels (guild_id text, channel0 text, channel1 text, channel2 text, channel3 text, channel4 text)''')
