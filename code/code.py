f66d0d95878aa9bc91b1b90fcaf26f4a9f4b26f5
Paillat <git@electronique.cc>
2023-04-01 12:53:15 +0200

    Update code.py

--- code/code.py
+++ code/code.py
@@ -1,10 +1,16 @@
 import discord 
+
 from discord import Intents
-import cogs # import the cogs
+
+import cogs
+
 from config import debug, discord_token
+
 intents = discord.Intents.default()
 intents.message_content = True
+
 bot = discord.Bot(intents=intents, help_command=None)
+
 bot.add_cog(cogs.Setup(bot))
 bot.add_cog(cogs.Settings(bot))
 bot.add_cog(cogs.Help(bot))
@@ -13,17 +19,10 @@ bot.add_cog(cogs.ManageChat(bot))
 bot.add_cog(cogs.Moderation(bot))
 
 @bot.event
-async def on_ready():
-    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="your messages to answer you"))
-    debug("Bot is ready")
-
-
-
-
-@bot.event
 async def on_application_command_error(ctx, error):
     debug(error)
     await ctx.respond(error, ephemeral=True)
+
 @bot.event
 async def on_ready():
     await bot.change_presence(
